04db6223bf6a7b002bb3eb175919ecf5a7757db3
K-sun-H <sunhe.dev@gmail.com>
2022-11-04 10:03:15 +0900

    mypage - itemlist

--- nemo/src/main/java/nemo/mapper/ItemMapper.java
+++ nemo/src/main/java/nemo/mapper/ItemMapper.java
@@ -9,6 +9,8 @@ public interface ItemMapper {
    
     List<ItemDto> selectItemList(String itemMaincategory) throws Exception;
     List<ItemDto> selectItemsubList(String itemSubcategory) throws Exception;
+    List<ItemDto> myItemList() throws Exception;
+    
     void updateReadCount(int itemNum) throws Exception;
     int insertItem(ItemDto item) throws Exception;
     ItemDto selectItemDetail(int itemNum) throws Exception;
