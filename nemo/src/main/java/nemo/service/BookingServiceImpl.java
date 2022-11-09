b62c1b92d7b9580b6b7a9fa718a39179d30aa5b9
K-sun-H <sunhe.dev@gmail.com>
2022-11-09 19:01:32 +0900

    booking insert

--- nemo/src/main/java/nemo/service/BookingServiceImpl.java
+++ nemo/src/main/java/nemo/service/BookingServiceImpl.java
@@ -2,19 +2,20 @@ package nemo.service;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Service;
 
 import nemo.dto.BookingDto;
 import nemo.dto.ItemDto;
 import nemo.mapper.BookingMapper;
 
+@Service
 public class BookingServiceImpl implements BookingService {
 	
 	@Autowired
 	private BookingMapper bookingMapper;
 	
-	 @Autowired
-     private ItemService service2;
+//	 @Autowired
+//     private ItemService service2;
 	
 	 //상품등록
 	   @Override
