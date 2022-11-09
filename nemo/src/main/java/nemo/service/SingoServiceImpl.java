4a8551f22888dfb0a57518d2d8d5679389f02f6c
yewon <uhbill@nate.com>
2022-11-09 19:59:21 +0900

    신고하기 진행중

---
+++ nemo/src/main/java/nemo/service/SingoServiceImpl.java
@@ -0,0 +1,22 @@
+package nemo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import nemo.dto.SingoDto;
+import nemo.mapper.SingoMapper;
+
+@Service
+public class SingoServiceImpl implements SingoService {
+	
+	@Autowired
+	private SingoMapper singoMapper;
+	
+	@Override
+	public int insertSingo(SingoDto singoDto) throws Exception {
+		System.out.println("서비스 타나??? 신고신고");
+		return singoMapper.insertSingo(singoDto);
+	}
+	
+
+}
