852c50ab92ba14d4302668d55aaefe7627069dac
yewon <uhbill@nate.com>
2022-11-09 19:59:21 +0900

    신고하기 진행중

---
+++ nemo/src/main/java/nemo/controller/RestSingoApiController.java
@@ -0,0 +1,29 @@
+package nemo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import nemo.dto.SingoDto;
+import nemo.service.SingoService;
+
+@Data
+@Slf4j
+@RestController
+@RequestMapping("/api")
+public class RestSingoApiController {
+	
+	@Autowired
+	private SingoService singoService;
+	
+	@RequestMapping(value = "/singo/take", method = RequestMethod.POST)
+	public int insertBooking(@RequestBody SingoDto singoDto) {
+		System.out.println(":::::::::::::::::::::::::::::::::::::::::::::::::"+singoDto);
+		return insertBooking(singoDto);
+	}
+
+}
---
+++ nemo/src/main/java/nemo/service/SingoService.java
@@ -0,0 +1,9 @@
+package nemo.service;
+
+import nemo.dto.SingoDto;
+
+public interface SingoService {
+
+	public int insertSingo(SingoDto singoDto) throws Exception;
+	
+}
