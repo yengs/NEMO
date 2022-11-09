51226919969735288e7ae64c5c3b8fa439cde355
DuMingK <fragrance_b@naver.com>
2022-11-09 15:05:52 +0900

    reviewDesc

--- nemo/src/main/java/nemo/mapper/ReviewMapper.java
+++ nemo/src/main/java/nemo/mapper/ReviewMapper.java
@@ -9,12 +9,23 @@ import nemo.dto.ReviewDto;
 
 @Mapper
 public interface ReviewMapper {
-	List<ReviewDto> selectMyReviewList(String reviewWriter) throws Exception; // 내가 작성한 리뷰 
+	
+	List<ReviewDto> selectMyReviewList(String reviewWriter) throws Exception; // 내가 작성한 리뷰
+
 	List<ReviewDto> selectYourReviewList(String reviewId) throws Exception; // 내 상품에 대한 리뷰
+
 	void insertReview(ReviewDto review) throws Exception; // 리뷰 작성
+
 	ReviewDto selectMyReviewDetail(int ReviewNum) throws Exception; // 내가 작성한 리뷰 상세
+
 	ReviewDto selectYourReviewDetail(int ReviewNum) throws Exception; // 내 상품에 대한 리뷰 상세
+
 	void updateReview(ReviewDto reviewDto) throws Exception; // 리뷰 업데이트
+
 	void deleteReview(int ReviewNum) throws Exception; // 리뷰 삭제
 	
+	// ------------------------ 
+	
+	
+
 }
