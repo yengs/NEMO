00e0108c4942e18ee86ca5b00288f2aafbe956a7
K-sun-H <sunhe.dev@gmail.com>
2022-11-10 16:24:30 +0900

    내대여이력(빌려줬어요,빌려왔어요)

--- nemo/src/main/java/nemo/service/BookingService.java
+++ nemo/src/main/java/nemo/service/BookingService.java
@@ -1,9 +1,15 @@
 package nemo.service;
 
+import java.util.List;
+
 import nemo.dto.BookingDto;
 
 public interface BookingService {
 
 	public void insertBooking(BookingDto booking) throws Exception; // 대여하기
+	
+	List<BookingDto> myLendList(String bookingItemwriter) throws Exception; //빌려줬어요
+	
+	public List<BookingDto> myBookingList(String bookingMember) throws Exception; //빌려왔어요
 	
 }
