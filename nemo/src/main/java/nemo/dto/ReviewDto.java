61e55e3eb229877b757459f2e3bdc3ef88110417
leessg <leesg812@gmail.com>
2022-11-14 18:56:08 +0900

    리뷰 변경

--- nemo/src/main/java/nemo/dto/ReviewDto.java
+++ nemo/src/main/java/nemo/dto/ReviewDto.java
@@ -7,7 +7,6 @@ public class ReviewDto {
     private int reviewNum;
     private String reviewWriter;
     private String reviewId;
-    private String reviewImage;
     private int reviewProductIdx;
     private String reviewContents;
     private int reviewSatisfaction;
