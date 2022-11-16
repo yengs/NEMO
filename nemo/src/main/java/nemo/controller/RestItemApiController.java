6bd4b18c2aaa82befeedf9c577b252af2e56b40f
yewon <uhbill@nate.com>
2022-11-16 17:43:43 +0900

    주간베스트아이템 조회 진행중

--- nemo/src/main/java/nemo/controller/RestItemApiController.java
+++ nemo/src/main/java/nemo/controller/RestItemApiController.java
@@ -38,6 +38,14 @@ public class RestItemApiController {
 	   return itemService.selectItemListByWeather(itemWeather);
    }
    
+   // 조회
+   @RequestMapping(value = "/item/best", method = RequestMethod.GET)
+   public List<ItemDto> selectItemListWeeklyBest() throws Exception {
+	   return itemService.selectItemListWeeklyBest();
+   }
+   
+//   selectItemListWeeklyBest
+   
    
    // 상품리스트 main 조회
    @RequestMapping(value = "/item/cate/{itemMaincategory}", method = RequestMethod.GET)
--- nemo/src/main/java/nemo/service/ItemService.java
+++ nemo/src/main/java/nemo/service/ItemService.java
@@ -11,6 +11,7 @@ public interface ItemService {
 
    public List<ItemDto> selectItemListRandom() throws Exception;
    public List<ItemDto> selectItemListByWeather(String itemWeather) throws Exception;
+   public List<ItemDto> selectItemListWeeklyBest() throws Exception;
    public List<ItemDto> selectItemList(String itemMaincategory) throws Exception;
    public List<ItemDto> selectItemsubList(String itemSubcategory) throws Exception;
    public List<ItemDto> myItemList(String itemWriter) throws Exception;
