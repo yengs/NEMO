84025b816c1b63bbcfa9ba51930343be6abf11e1
Hyemie <hyemhamgoa1111@gmail.com>
2022-11-21 18:46:01 +0900

    신고하기 중간 저장

--- nemo/src/main/java/nemo/mapper/SingoMapper.java
+++ nemo/src/main/java/nemo/mapper/SingoMapper.java
@@ -10,7 +10,7 @@ import nemo.dto.SingoDto;
 @Mapper
 public interface SingoMapper {
 	
-	// 신고 insert
+	// 유저 - 신고 insert
 	int insertSingo(SingoDto singoDto) throws Exception;
 
 	// 피신고자 이름 가져오기
@@ -19,6 +19,16 @@ public interface SingoMapper {
 	// 관리자페이지로 피신고자 이름 가져오기
 	List<SingoDto> ReasonNwriter(String memberId) throws Exception;
 
+	// 관리자 - 신고 접수
+	void confirmWarn(SingoDto singoDto) throws Exception;
+	
 	// 신고 상세보기 가져오기
 	List<SingoDto> selectDetail(int singoNum) throws Exception;
+
+	// 관리자 신고 취소
+	int deleteWarn(int singoNum) throws Exception;
+
+	// 신고 5번 이상 회원 정지
+	void deleteMember(int singoNum) throws Exception;
+
 }
